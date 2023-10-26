f17522fbec68adfbb3e6386add60cb6c08a10c39
zhangjunkai <250336272@qq.com>
2023-10-26 20:34:52 +0800

    chore: 集成pinia

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 app.mount("#app");
