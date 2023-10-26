49ce8f8de3fc0c03695b21e2fe98c712beea827f
zhangjunkai <250336272@qq.com>
2023-10-26 21:08:50 +0800

    feat: 分发票权

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,10 @@ declare module 'vue' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    VanButton: typeof import('vant/es')['Button']
+    VanCellGroup: typeof import('vant/es')['CellGroup']
+    VanField: typeof import('vant/es')['Field']
+    VanForm: typeof import('vant/es')['Form']
     VanNavBar: typeof import('vant/es')['NavBar']
     VanTabbar: typeof import('vant/es')['Tabbar']
     VanTabbarItem: typeof import('vant/es')['TabbarItem']
--- src/views/Mandate.vue
+++ src/views/Mandate.vue
@@ -1,3 +1,37 @@
 <template>
-  <div>mandate</div>
+  <van-form @submit="handleMandate">
+    <van-cell-group inset>
+      <van-field
+        v-model="owner"
+        name="主持人"
+        label="主持人"
+        placeholder="主持人"
+        disabled="true"
+      />
+      <van-field
+        v-model="mandateAddress"
+        type="textarea"
+        rows="5"
+        autosize
+        name="投票人地址"
+        label="投票人地址"
+        placeholder="投票人地址"
+        :rules="[{ required: true, message: '请填写投票人地址' }]"
+      />
+    </van-cell-group>
+    <div style="margin: 16px">
+      <van-button round block type="primary" native-type="submit">
+        分发票权
+      </van-button>
+    </div>
+  </van-form>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const owner = ref("123");
+const mandateAddress = ref("");
+
+const handleMandate = () => {};
+</script>
